05421ee78f283146f2e9ee907872ad6a660e010b
tatsukiasd <62208113+tatsukiasd@users.noreply.github.com>
2021-05-08 13:05:53 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,24 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  console.log(data);
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
